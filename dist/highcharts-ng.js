2e1c1fdfedd7e8bb92bc4579904e164359765019
patiphannn <patiphann@gmail.com>
2017-12-20 14:57:15 +0700

    Fix error Chart undefined of null

--- dist/highcharts-ng.js
+++ dist/highcharts-ng.js
@@ -17,12 +17,9 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
 
   if (window && window.Highcharts) {
     Highcharts = window.Highcharts;
-  } else if (typeof module !== 'undefined' && typeof exports !== 'undefined' &&
-    module.exports === exports && module.exports === 'highcharts-ng'
-  ) {
-        Highcharts = require('highcharts');
+  } else if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.exports === 'highcharts-ng') {
+    Highcharts = require('highcharts');
   }
-
 
   angular.module('highcharts-ng', [])
     .component('highchart', {
